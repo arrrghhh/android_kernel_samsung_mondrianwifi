ee79708b5c5745aa068ec974616d65aaf32b0320
arrrghhh <arrrghhh@xdandroid.com>
2014-06-23 23:07:54 -0600

    Initial bringup

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 34 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#34 SMP PREEMPT Thu May 22 23:11:04 PDT 2014"
+#define LINUX_COMPILE_BY "freeza"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
